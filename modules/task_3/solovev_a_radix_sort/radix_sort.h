6eacd07282e63baa382194be9fb47dc5af2b2f34
Alexandr-Solovev <n19ht1n9ale27@gmail.com>
2021-05-15 16:55:41 +0300

    first commit

---
+++ modules/task_3/solovev_a_radix_sort/radix_sort.h
@@ -0,0 +1,16 @@
+// Copyright 2021 Solovev Aleksandr
+#ifndef MODULES_TASK_3_SOLOVEV_A_RADIX_SORT_RADIX_SORT_H_
+#define MODULES_TASK_3_SOLOVEV_A_RADIX_SORT_RADIX_SORT_H_
+
+#include <omp.h>
+#include <time.h>
+#include <stdlib.h>
+#include <random>
+#include <vector>
+
+int generateRandomArray(std::vector<int> *buffer, int min, int max);
+int SortingCheck(std::vector<int> *buffer);
+int RadixSort(std::vector<int> *buffer);
+int ParallelSortingTBB(std::vector<int> *buffer);
+
+#endif  // MODULES_TASK_3_SOLOVEV_A_RADIX_SORT_RADIX_SORT_H_
