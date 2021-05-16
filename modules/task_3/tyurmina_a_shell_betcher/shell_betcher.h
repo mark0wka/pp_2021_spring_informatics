f5d0017cf53a6383f8c09310bcd53bcaf01a8c70
Alexandra Tyurmina <sasha.tyurmina@gmail.com>
2021-05-16 19:54:25 +0300

    test3

--- modules/task_3/tyurmina_a_shell_betcher/shell_betcher.h
+++ modules/task_3/tyurmina_a_shell_betcher/shell_betcher.h
@@ -7,7 +7,7 @@
 #include <utility>
 #include <functional>
 
-std::vector<int> Random(int size); 
+std::vector<int> Random(int size);
 std::vector<std::vector<int>> Separat(const std::vector<int>& massiv, size_t n);
 std::vector<int> ShellSort(const std::vector<int>&, int size);
 std::vector<int> BetcherEven(const std::vector<int>&, const std::vector<int>&);
@@ -17,4 +17,4 @@ std::vector<int> ShellBetcherSeq(const std::vector<int>&, const int, int);
 std::vector<int> ShellBetcherMerge(const std::vector<std::vector<int>>&, const int, int);
 std::vector<int> MergeTBB(const std::vector<int>&, const int, int);
 
-#endif  // MODULES_TASK_2_TYURMINA_A_SHELL_BETCHER_SHELL_BETCHER_H_
+#endif  // MODULES_TASK_3_TYURMINA_A_SHELL_BETCHER_SHELL_BETCHER_H_
