8ee55065c97a1229a9dc76bd58253b2065330c35
KasjanychevMikhail <43135863+KasjanychevMikhail@users.noreply.github.com>
2021-05-19 00:11:15 +0300

    task3ver2

--- modules/task_3/kasjanychev_m_conjugate_gradient/conjugate_gradient.cpp
+++ modules/task_3/kasjanychev_m_conjugate_gradient/conjugate_gradient.cpp
@@ -143,8 +143,7 @@ std::vector<std::vector<double> > searchReverseMatrix(std::vector<std::vector<do
         for (int j = 0; j < size; j++) {
             if (i == j) {
                 E[i][j] = 1.0;
-            }
-            else {
+            } else {
                 E[i][j] = 0.0;
             }
         }
