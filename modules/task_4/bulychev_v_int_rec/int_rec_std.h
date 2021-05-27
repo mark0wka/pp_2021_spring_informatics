ef4bbf9d4213419cb4512b4b1b564d7d7505f260
MG_PC <vlad-hk.19.03@mail.ru>
2021-05-27 21:37:19 +0300

    done

---
+++ modules/task_4/bulychev_v_int_rec/int_rec_std.h
@@ -0,0 +1,15 @@
+// Copyright 2021 Bulychev Vladislav
+#ifndef MODULES_TASK_4_BULYCHEV_V_INT_REC_INT_REC_STD_H_
+#define MODULES_TASK_4_BULYCHEV_V_INT_REC_INT_REC_STD_H_
+
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <algorithm>
+
+double Calculation_Seq(std::vector<double> a, std::vector<double> b,
+    int n, double(*f)(std::vector<double>));
+double Calculation_Std(std::vector<double> a, std::vector<double> b,
+    int n, double(*f)(std::vector<double>));
+
+#endif  // MODULES_TASK_4_BULYCHEV_V_INT_REC_INT_REC_STD_H_
